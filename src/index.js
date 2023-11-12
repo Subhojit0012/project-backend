765613f19065530c577bf7f149a6ebe7d4dd73ad
Subhojit0012 <subhojitroy00012@gmail.com>
2023-11-12 11:29:53 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@
 // import { DB_NAME } from "./containts";
 import dotenv from "dotenv";
 import connectDB from "./db/data.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection falied !!", err);
+  });
 
 /*
 import express from "express";
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (request) => {
+  (req, res, next) => {
+    Promise.resolve(request(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = (fn)=> async (req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+//     } catch(error){
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
